ff295bcd63d124f48da33f5418d961f7a02175d5
EverFelliphe <123915570+EverFelliphe@users.noreply.github.com>
2023-05-31 14:46:38 -0300

    TESTE

--- Frontend/assets/js/pagina_inicial.js
+++ Frontend/assets/js/pagina_inicial.js
@@ -10,11 +10,12 @@ $(document).ready(function(){
         */
         data.forEach(value =>{
             
-            $('#lista_projetos').append(` <tr>
-            <td  class="border   border-black" style="border-radius:24px;"><a href="/relatorio?id_relatorio=${value.id}" class="navbar-brand ms-3">Projeto ${i}
-    </td>
+            $('#lista_projetos').append(`
+            <li   class=" w-100 list-unstyled btn d-flex align-items-center "  style="box-shadow:  26px 26px 51px #c2c2c2;
+            8px 8px 21px #ffffff;;border-radius:12px;background-color:#fafafa;height:52px ; margin-bottom:-12px;" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-custom-class="custom-tooltip" data-bs-title="This top tooltip is themed via CSS variables."><a href="/relatorio?id_relatorio=${value.id}" style="font-size:16px;" class="navbar-brand ibm ms-3">PROJETO ${i}
+    </li>   
     
-            </tr>`)
+            `)
             i++
         })
     })
